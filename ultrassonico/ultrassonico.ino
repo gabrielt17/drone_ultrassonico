09fa7d39701264a22f0001eee656ea1fe21854e3
gabriel <gabriel.muniz@ufv.br>
2023-11-05 19:15:30 -0300

    Corrigi um erro de grafia no comentário!

--- ultrassonico/ultrassonico.ino
+++ ultrassonico/ultrassonico.ino
@@ -13,7 +13,7 @@ Data de término: xx/xx/xxxx
 
 
   Serial.begin(9600);
-	pinMode(trig, OUTPUT); //Cometário de exemplo para a Cleide
+	pinMode(trig, OUTPUT); //Comentário de exemplo para a Cleide
 	pinMode(echo, INPUT);
 
 }
