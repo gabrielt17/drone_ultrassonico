abbea0a4b0123f7daf1dfc0d490e482c3425c342
Lucastm1 <149830892+Lucastm1@users.noreply.github.com>
2023-11-07 16:30:54 -0300

    Update mpu6050.ino

--- mpu6050/mpu6050.ino
+++ mpu6050/mpu6050.ino
@@ -1,10 +1,10 @@
-include <Wire.h>
+#include <Wire.h>
 
 float eY, eX, eZ; // Taxas de variação nos Eixos X, Y e Z (Z vertical)
 
 void sinalGiroscopio(void) {    // Inicia comunicação com sensor no seu
   
-  // Escreve a configuração de filtor passa baixo para 10 Hz
+  // Escreve a configuração de filtro passa baixo para 10 Hz
   Wire.beginTransmission(0x68);
   Wire.write(0x1A);
   Wire.write(0x05);
