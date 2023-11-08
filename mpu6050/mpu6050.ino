5b8486d74cbdaabe6bd473d936e983035c7ac34f
gabrielt17 <gabriel.muniz@ufv.br>
2023-11-08 17:56:08 -0300

    Adicionado linhas de código para configuração incial de sensor e algumas links importantes

--- mpu6050/mpu6050.ino
+++ mpu6050/mpu6050.ino
@@ -4,7 +4,10 @@ Data de início: 08/11/2023
 Data de término: xx/xx/xxxx
 Descrição: Código que utiliza do sensor MPU6065 para mensurar o ângulo
 de rotação a partir de uma determinada origem.
-Documentação biblioteca: https://adafruit.github.io/Adafruit_MPU6050/html/class_adafruit___m_p_u6050.html
+Documentação da biblioteca: https://adafruit.github.io/Adafruit_MPU6050/html/class_adafruit___m_p_u6050.html
+Datasheet MPU6050: https://invensense.tdk.com/wp-content/uploads/2015/02/MPU-6000-Datasheet1.pdf 
+Mapa de registadores: https://cdn.sparkfun.com/datasheets/Sensors/Accelerometers/RM-MPU-6000A.pdf
+Código exemplo configurando registadores manualmente: https://www.makerhero.com/blog/tutorial-acelerometro-mpu6050-arduino/
 */
 
 #include <Adafruit_MPU6050.h>
@@ -23,8 +26,12 @@ void setup (void) {
     delay(2000);
   }
   Serial.println("MPU encontrado");
+  Serial.println("");
   // Algumas configurações do sensor
-  mpu.setAccelerometerRange(MPU6050_RANGE_4_G)
+  // Mede acelerações de até 4G
+  mpu.setAccelerometerRange(MPU6050_RANGE_4_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG); // Mede giros de até 500º/s
+  mpu.setFilterBandwidth(MPU6050_BAND_10_HZ); // Filtro passa baixo de 10Hz
 }
 
 
