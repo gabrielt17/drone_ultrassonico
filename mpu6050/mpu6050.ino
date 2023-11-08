fbe31a8cb6c3b4f663d01371afdb0fc87fd6dcd0
gabrielt17 <gabriel.muniz@ufv.br>
2023-11-08 17:32:56 -0300

    Código MPU6050 reescrito do zero utilizado a biblioteca Adafruit_MPU6050

--- mpu6050/mpu6050.ino
+++ mpu6050/mpu6050.ino
@@ -1,14 +1,33 @@
-include <Wire.h>
+/*
+Autores(as): Cleide Almeida, Gabriel Víctor, Lucas Tavares
+Data de início: 08/11/2023
+Data de término: xx/xx/xxxx
+Descrição: Código que utiliza do sensor MPU6065 para mensurar o ângulo
+de rotação a partir de uma determinada origem.
+Documentação biblioteca: https://adafruit.github.io/Adafruit_MPU6050/html/class_adafruit___m_p_u6050.html
+*/
 
-float eY, eX, eZ; // Taxas de variação nos Eixos X, Y e Z (Z vertical)
+#include <Adafruit_MPU6050.h>
 
-void sinalGiroscopio(void) {    // Inicia comunicação com sensor no seu
-  
-  // Escreve a configuração de filtor passa baixo para 10 Hz
-  Wire.beginTransmission(0x68);
-  Wire.write(0x1A);
-  Wire.write(0x05);
-  Wire.endTransmission();
+// Apelido para classe da biblioteca do MPU6050
+Adafruit_MPU6050 mpu;
 
+void setup (void) {
+  Serial.begin(9600);
+  // Enquanto monitor serial não for aberto, pausar
+  while(!Serial)
+    delay(10);
+  // Tenta encontrar e iniciar o sensor
+  if (!mpu.begin()) {
+    Serial.println("Não foi possível iniciar o sensor");
+    delay(2000);
+  }
+  Serial.println("MPU encontrado");
+  // Algumas configurações do sensor
+  mpu.setAccelerometerRange(MPU6050_RANGE_4_G)
+}
+
+
+void loop () {
 
 }
