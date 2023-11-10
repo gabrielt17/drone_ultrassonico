a5b02b38374c62275cd3927fdee332c010bce6d6
lucastm1 <lucas.tavares@ufv.br>
2023-11-10 15:00:06 -0300

    Novo código para mpu

--- mpu6050/mpu6050.ino
+++ mpu6050/mpu6050.ino
@@ -1,16 +1,44 @@
 #include <Wire.h>
-
-float eY, eX, eZ; // Taxas de variação nos Eixos X, Y e Z (Z vertical)
-
-void sinalGiroscopio(void) {    // Inicia comunicação com sensor no seu
-  
-  // Escreve a configuração de filtro passa baixo para 10 Hz
+float RateRoll, RatePitch, RateYaw;
+void gyro_signals(void) {
   Wire.beginTransmission(0x68);
   Wire.write(0x1A);
   Wire.write(0x05);
+  Wire.endTransmission(); 
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1B); 
+  Wire.write(0x8); 
+  Wire.endTransmission(); 
+  Wire.beginTransmission(0x68);
+  Wire.write(0x43);
   Wire.endTransmission();
-
-  
-
-
+  Wire.requestFrom(0x68,6);
+  int16_t GyroX=Wire.read()<<8 | Wire.read();
+  int16_t GyroY=Wire.read()<<8 | Wire.read();
+  int16_t GyroZ=Wire.read()<<8 | Wire.read();
+  RateRoll=(float)GyroX/65.5;
+  RatePitch=(float)GyroY/65.5;
+  RateYaw=(float)GyroZ/65.5;
+}
+void setup() {
+  Serial.begin(57600);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  Wire.setClock(400000);
+  Wire.begin();
+  delay(250);
+  Wire.beginTransmission(0x68); 
+  Wire.write(0x6B);
+  Wire.write(0x00);
+  Wire.endTransmission();
+}
+void loop() {
+  gyro_signals();
+  Serial.print("Roll rate [°/s]= ");
+  Serial.print(RateRoll);
+  Serial.print(" Pitch Rate [°/s]= ");
+  Serial.print(RatePitch);
+  Serial.print(" Yaw Rate [°/s]= ");
+  Serial.println(RateYaw);
+  delay(50);
 }
