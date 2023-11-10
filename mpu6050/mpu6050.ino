f9aab77fdcd4a5fb3a68d3a7d069be59ba4070ec
gabrielt17 <gabriel.muniz@ufv.br>
2023-11-10 16:40:33 -0300

    Adicionada feedback pela porta serial

--- mpu6050/mpu6050.ino
+++ mpu6050/mpu6050.ino
@@ -1,8 +1,8 @@
 /*
 Autores(as): Cleide Almeida, Gabriel Víctor, Lucas Tavares
 Data de início: 08/11/2023
-Data de término: xx/xx/xxxx
-Descrição: Código que utiliza do sensor MPU6065 para mensurar o ângulo
+Última atualização: xx/xx/xxxx
+Código que utiliza do sensor MPU6065 para mensurar o ângulo
 de rotação a partir de uma determinada origem.
 Documentação da biblioteca: https://adafruit.github.io/Adafruit_MPU6050/html/class_adafruit___m_p_u6050.html
 Datasheet MPU6050: https://invensense.tdk.com/wp-content/uploads/2015/02/MPU-6000-Datasheet1.pdf 
@@ -11,6 +11,7 @@ Código exemplo configurando registadores manualmente: https://www.makerhero.com
 */
 
 #include <Adafruit_MPU6050.h>
+#include <math.h>
 
 // Apelido para classe da biblioteca do MPU6050
 Adafruit_MPU6050 mpu;
@@ -28,13 +29,35 @@ void setup (void) {
   Serial.println("MPU encontrado");
   Serial.println("");
   // Algumas configurações do sensor
-  // Mede acelerações de até 4G
-  mpu.setAccelerometerRange(MPU6050_RANGE_4_G);
+  mpu.setAccelerometerRange(MPU6050_RANGE_4_G); // Mede acelerações de até 4G
   mpu.setGyroRange(MPU6050_RANGE_500_DEG); // Mede giros de até 500º/s
   mpu.setFilterBandwidth(MPU6050_BAND_10_HZ); // Filtro passa baixo de 10Hz
+  Serial.println("Configuração inicial feita! Iniciando\
+  código principal");
 }
 
 
 void loop () {
+  // Cria ponteiros que receberão os
+  // valores da aceleração e do giroscópio
+  sensors_event_t accel, gyro, temp;
+  mpu.getEvent(&accel, &gyro, &temp);
 
+  // Imprime os dados
+  Serial.print("Aceleração (x, y,z)[m/s^2]: ");
+  Serial.print(accel.acceleration.x);
+  Serial.print(", ");
+  Serial.print(accel.acceleration.y);
+  Serial.print(", ");
+  Serial.println(accel.acceleration.z);
+
+  // Serial.print("Rotação (x, y, z)[º/s]: ");
+  // Serial.print((gyro.gyro.x*360)/(2*M_PI));
+  // Serial.print(", ");
+  // Serial.print((gyro.gyro.y*360)/(2*M_PI));
+  // Serial.print(", ");
+  // Serial.println((gyro.gyro.z*360)/(2*M_PI));
+  
+  Serial.println("");
+  delay(100);
 }
